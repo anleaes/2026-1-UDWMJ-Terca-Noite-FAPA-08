827e0ce5853cba3fb37f1104ae5a52fe18e47a24
FerreiraRAf <rafaferreirarosa941@gmail.com>
2026-06-03 22:59:06 -0300

    Adiciona uma nova classe Nutricionistas

--- academia/settings.py
+++ academia/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
     'categoria_alimento.apps.CategoriaAlimentoConfig',
+    'nutricionistas.apps.NutricionistasConfig',
 ]
 
 MIDDLEWARE = [
--- academia/urls.py
+++ academia/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
+    path('nutricionistas/', include('nutricionistas.urls', namespace='nutricionistas')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
