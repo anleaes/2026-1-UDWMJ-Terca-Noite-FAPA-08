25e3629f7732ada03e9c8623a6b6775231f9d4eb
Willian <willianlds.02@gmail.com>
2026-06-03 22:15:59 -0300

    Classe grupo muscular

--- academia/settings.py
+++ academia/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
     'categoria_alimento.apps.CategoriaAlimentoConfig',
+    'gruposmusculares.apps.GruposmuscularesConfig',
 ]
 
 MIDDLEWARE = [
--- academia/urls.py
+++ academia/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
+    path('grupos-musculares/', include('gruposmusculares.urls', namespace='gruposmusculares')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
